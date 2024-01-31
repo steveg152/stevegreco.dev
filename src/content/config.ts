7bef829ff6fcbfc68556dc4f5664ac0c68937ab0
steveg152 <steve.greco2@gmail.com>
2024-01-31 15:49:08 -0500

    feat: added content collection for projects

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,33 +1,17 @@
 // 1. Import utilities from `astro:content`
 import { defineCollection, z } from 'astro:content'
-const skillSchema = z.object({
-  title: z.string(),
-  primary: z.boolean(),
-})
-// 2. Define your collection(s)
-const skillsCollection = defineCollection({
+
+const projectCollection = defineCollection({
   type: 'data',
   schema: z.object({
     title: z.string(),
-    items: z.array(skillSchema),
-    order: z.number(),
+    description: z.string(),
+    thumbnail: z.string(),
+    repo_url: z.string(),
   }),
-  /* ... */
-})
-
-const expCollection = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    summary: z.string(),
-    skills: z.array(z.string()),
-    moreInfo: z.boolean(),
-  }),
-  /* ... */
 })
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  skills: skillsCollection,
-  experience: expCollection,
+  projects: projectCollection,
 }
